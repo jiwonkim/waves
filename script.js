a091bc65625771c250dabb9ac5ca5ef62561d570
Jiwon Kim <kittyvision@gmail.com>
2015-04-12 15:11:35 -0700

    Can display multiple waves that respond to mouse drag

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-var settings = {
+var config = {
     c: 0.2, // wave equation constant
     color: '#9af'
 }
@@ -12,64 +11,71 @@ $(document).ready(function() {
     var dt = 0.01; // simulation step size in seconds
 
     var n = 100; // number of samples
-    var u = new Float32Array(n); // value of wave at each x
-    var u_t = new Float32Array(n); // velocity at each x
-    var u_x = new Float32Array(n - 1);
-    var u_tt = new Float32Array(n - 2);
 
-    var isDragging = false;
-    var x0, y0, dy;
+    var dragState = {
+        isDragging: false,
+        x0: 0,
+        y0: 0,
+        dy: 0
+    }
     $(canvas)
         .mousedown(function(evt) {
-            isDragging = true;
-            x0 = evt.offsetX;
-            y0 = evt.offsetY;
+            dragState.isDragging = true;
+            dragState.x0 = evt.offsetX;
+            dragState.y0 = evt.offsetY;
         })
         .mousemove(function(evt) {
-            if (isDragging) {
-                dy = evt.offsetY - y0;
-                y0 = evt.offsetY;
+            if (dragState.isDragging) {
+                dragState.dy = evt.offsetY - dragState.y0;
+                dragState.y0 = evt.offsetY;
             }
         })
         .on('mouseup mouseout mouseleave', function(evt) {
-            isDragging = false;
+            dragState.isDragging = false;
         });
 
-    function bigWave(i, crestIdx) {
-        var amplitude, period, phaseShift;
-        var dampener, lengthener;
-
-        // dampened further from the crest, but only for upward curves 
-        dampener = Math.pow(0.97, Math.abs(crestIdx - i))
-        amplitude = 0.8 * n * dampener;
-
-        // period for big wave spans half the length of the pool, but
-        // lengthens further away from the crest
-        lengthener = Math.pow(1.001, Math.abs(crestIdx - i));
-        period = 0.5 * n * lengthener;
-
-        // crestIdx = phaseShift + .25 * period
-        phaseShift = crestIdx - .25 * period;
-
-        // value of u at i (wave's height)
-        return amplitude * Math.sin((2.0 * Math.PI / period) * (i - phaseShift));
-    }
-
-    function smallWave(i, crestIdx) {
-        var amplitude, period, phaseShift, crestOffset;
-        
-        // small amplitude for small wave, dampened further from the crest
-        amplitude = 0.1 * n * Math.pow(0.99, Math.abs(crestIdx - i));
-
-        // period for big wave spans a fourth of the length of the pool
-        period = 0.25 * n;
-
-        // the crest isn't quite centered for the small wave
-        crestOffset = 0.05 * n;
-        phaseShift = crestIdx - .25 * period - crestOffset;
-
-        return amplitude * Math.sin((2.0 * Math.PI / period) * (i - phaseShift));
-    }
+    var waves = [
+        wave(
+            canvas,
+            n,
+            [
+                {
+                    amplitude: 0.8,
+                    period: 0.5,
+                    dampener: 0.97,
+                    lengthener: 1.001,
+                    crestOffset: 0
+                },
+                {
+                    amplitude: 0.1,
+                    period: 0.25,
+                    dampener: 0.99,
+                    lengthener: 1.0,
+                    crestOffset: 0.2
+                }
+            ]
+        ),
+        wave(
+            canvas,
+            n,
+            [
+                {
+                    amplitude: 0.5,
+                    period: 0.8,
+                    dampener: 0.9,
+                    lengthener: 1.001,
+                    crestOffset: 0
+                },
+                {
+                    amplitude: 0.1,
+                    period: 0.2,
+                    dampener: 0.99,
+                    lengthener: 1.0,
+                    crestOffset: -0.2
+                }
+            ]
+        ),
+    ];
 
     requestAnimationFrame(frame);
 
@@ -77,27 +83,53 @@ $(document).ready(function() {
         physics();
         render();
         requestAnimationFrame(frame);
-
-        // DEBUG
-        if (isDragging) {
-            context.fillStyle = 'black';
-            context.beginPath();
-            context.moveTo(x0, 0);
-            context.lineTo(x0, canvas.height);
-            context.closePath();
-            context.stroke();
-        }
     }
 
     function physics() {
         var t_now = new Date().getTime() / 1000 - t_0; // time in seconds
         while(t < t_now) {
-            physicsStep();
+            waves.forEach(function(waveInstance) {
+                waveInstance.physics(dt, dragState);
+            });
             t += dt;
         }
     }
 
-    function physicsStep() {
+    function render() {
+        context.fillStyle = 'rgba(153, 170, 255, 0.5)';
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        var base = 0.5 * canvas.height;
+        var unitWidth = canvas.width / n; // dx per sample in pixels
+
+        // TODO for all waves, display
+        waves.forEach(function(waveInstance) {
+            var u = waveInstance.heightMap();
+
+            context.beginPath();
+            context.moveTo(0, canvas.height);
+            for(var i = 0; i < n; i++) {
+                var x = i * unitWidth;
+                var h = u[i];
+                context.lineTo(x, base - h);
+            }
+            context.lineTo(canvas.width, canvas.height);
+            context.closePath();
+            context.fill();
+        });
+    };
+});
+
+var wave = function(canvas, n, waveSettings) {
+    var _canvas = canvas,
+        _n = n, // number of samples
+        _waves = waveSettings; // list of wave settings
+
+    var u = new Float32Array(n); // value of wave at each sample idx
+    var u_t = new Float32Array(n); // velocity at each sample idx
+    var u_x = new Float32Array(n - 1);
+    var u_tt = new Float32Array(n - 2);
+
+    var _physics = function(dt, dragState) {
         var dx = 1. / n; // change in x per value in u
 
         // compute dx for each increment of x
@@ -105,7 +137,7 @@ $(document).ready(function() {
             u_x[i] = (u[i + 1] - u[i]) / dx;
         }
 
-        var c2 = settings.c * settings.c; // Constant C^2
+        var c2 = config.c * config.c; // Constant C^2
         for (var i = 0; i < n - 2; i++) {
             // compute second derivative wrt x
             var u_xx_i = (u_x[i + 1] - u_x[i]) / dx;
@@ -130,31 +162,47 @@ $(document).ready(function() {
         u[0] = u[1];
 
         // update u while being dragged
-        if (isDragging && x0 && dy) {
-            var crestIdx = x0 * n / canvas.width;
+        if (dragState.isDragging) {
+            var crestIdx = dragState.x0 * n / canvas.width;
             for(var i = 0; i < n; i++) {
                 // positive dy means wave being pushed down, so
                 // subtract artificial wave multiplied by dy
-                u[i] -= 0.002 * dy * (bigWave(i, crestIdx) + smallWave(i, crestIdx));
+                var offset = 0; 
+                _waves.forEach(function(settings) {
+                    offset += _compute(i, crestIdx, settings);
+                });
+                u[i] -= 0.002 * dragState.dy * offset;
             }
         }
-    }
-
-    function render() {
-        context.fillStyle = settings.color;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        var base = 0.5 * canvas.height;
-        var unitWidth = canvas.width / n; // dx per sample in pixels
-        context.beginPath();
-        context.moveTo(0, canvas.height);
-        for(var i = 0; i < n; i++) {
-            var x = i * unitWidth;
-            var h = u[i];
-            context.lineTo(x, base - h);
-        }
-        context.lineTo(canvas.width, canvas.height);
-        context.closePath();
-        context.fill();
     };
-});
 
+    var _compute = function(i, crestIdx, settings) {
+        var amplitude, period, phaseShift, crestOffset;
+        var dampener, lengthener;
+
+        // dampened further from the crest
+        dampener = Math.pow(settings.dampener, Math.abs(crestIdx - i))
+        amplitude = settings.amplitude * n * dampener;
+
+        // period for big wave spans half the length of the pool, but
+        // lengthens further away from the crest
+        lengthener = Math.pow(settings.lengthener, Math.abs(crestIdx - i));
+        period = settings.period * n * lengthener;
+
+        // crestIdx = phaseShift + .25 * period
+        crestOffset = settings.crestOffset * n;
+        phaseShift = crestIdx - .25 * period - crestOffset;
+
+        // value of u at i (wave's height)
+        return amplitude * Math.sin((2.0 * Math.PI / period) * (i - phaseShift));
+    };
+
+    var _heightMap = function() {
+        return u;
+    };
+
+    return {
+        physics: _physics,
+        heightMap: _heightMap,
+    }
+};
