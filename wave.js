cecb3d64fb1483d3e4c4a5dabf581e4ff85c530a
Jiwon Kim <kittyvision@gmail.com>
2015-04-29 00:03:05 -0700

    Added docstrings

--- wave.js
+++ wave.js
@@ -13,42 +13,30 @@
  * @property {number} period
  */
 
-/**
- * @typedef {Object} SplashState
- * @property {boolean} isSplashing
- * @property {number} x - the x coordinate of the splash's origin
- * @property {number} vy - the y velocity of the splash at splash origin
- */
 
-var defaultWaveSettings = {
-    numSamples: 100,
-    splashiness: 0.01,
-    damping: 0.995,
-    children: [
-        {period: 0.25, amplitude: 0.2},
-        {period: 0.5, amplitude: 0.6},
-        {period: 0.3, amplitude: 0.2}
-    ],
-}
+var DEFAULT_NUM_SAMPLES = 100;
+var DEFAULT_SPLASHINESS = 0.01;
+var DEFAULT_DAMPING = 0.995;
+var DEFAULT_CHILDREN = [
+    {period: 0.25, amplitude: 0.2},
+    {period: 0.5, amplitude: 0.6},
+    {period: 0.3, amplitude: 0.2}
+];
 
 /**
  * Defines a single wave with the given settings.
- * @param {number} n - The number of samples
- * @param {number} waveEquationConstant -  bigger the constant, the
- *      faster the wave propagates
- * @param {Array.<WaveSettings>} waveSettings - the final wave is a
- *      sum of partial waves, defined by these settings
+ * @param {WaveSettings=} settings
  */
 function wave(settings) {
     if (settings === undefined) {
-        settings = defaultWaveSettings;
+        settings = {}
     }
 
     // WAVE SETTINGS
-    var _n = settings.numSamples || defaultSettings.numSamples;
-    var _children = _normalizeAmplitudes(settings.children) || defaultWaveSettings.children;
-    var _splashiness = settings.splashiness || defaultSettings.splashiness;
-    var _damping = settings.damping || defaultSettings.damping;
+    var _n = settings.numSamples || DEFAULT_NUM_SAMPLES;
+    var _children = _normalizeAmplitudes(settings.children) || DEFAULT_CHILDREN;
+    var _splashiness = settings.splashiness || DEFAULT_SPLASHINESS;
+    var _damping = settings.damping || DEFAULT_DAMPING;
     
     // THE SWELL
     var _swell = swell();
@@ -59,11 +47,16 @@ function wave(settings) {
     var u_x = new Float32Array(_n - 1);
     var u_tt = new Float32Array(_n - 2);
 
+
     /*-----------------*/
     /* PRIVATE METHODS */
     /*-----------------*/
 
     function _normalizeAmplitudes(children) {
+        if (!children) {
+            return;
+        }
+
         var totalAmplitude = 0;
         children.forEach(function(childWaveSettings) {
             totalAmplitude += childWaveSettings.amplitude;
@@ -73,23 +66,52 @@ function wave(settings) {
         });
     }
 
+    /**
+     * Advance the swell in the wave by adding up the child waves
+     */
     function _gain() {
-        var sum = 0;
+        var totalVolume = 0;
+
+        // Compute the height of each child wave at each sample
+        // and add the offset to the current height map
         for(var i = 0; i < _n; i++) {
-            var offset = 0; 
             _children.forEach(function(childWaveSettings) {
-                offset += _childHeight(i, childWaveSettings);
+                u[i] += _childHeight(i, childWaveSettings);
             });
-            u[i] += offset;
-            sum += u[i];
+
+            totalVolume += u[i];
         }
 
-        // Make sure the wave level stays the same
-        var avg = sum / _n;
+        // Make sure the wave level stays the same. The sum of all
+        // heights at each sample should be 0, at the neutral level.
+        var averageVolume = totalVolume / _n;
         for (var i = 0; i < _n; i++) {
-            u[i] -= avg;
+            u[i] -= averageVolume;
         }
     }
+
+    /**
+     * Computes the height of the wave at sample i.
+     *
+     * @private
+     * @param {number} i - index of the sample to compute the wave height for
+     * @param {nubmer} crestIdx - index of the crest of the wave
+     * @param {WaveSettings} settings
+     */
+    function _childHeight(i, childWaveSettings) {
+        // x position of the given sample idx as a float in [0, 1]
+        var x = i / _n;
+
+        // sine wave params
+        var amplitude = childWaveSettings.amplitude;
+        var period = childWaveSettings.period;
+        var phaseShift = _swell.phaseShift();
+
+        // returns the y value of the child wave at sample i, shifted so that
+        // a peak occurs at the current well's index
+        return _swell.strength() * amplitude * Math.sin((2 * Math.PI / period) * (x - phaseShift));
+    };
+
 
     /*-----------------*/
     /*  PUBLIC METHODS */
@@ -142,50 +164,49 @@ function wave(settings) {
     };
 
     /**
+     * Create a splash in the wave.
      *
+     * @param {number} x - the x position of the splash. Float in [0, 1]
+     * @param {number} strength - the strength of the splash. Float in [0, 10]
+     * @param {boolean} isPositive - if true, then it's an upward splash.
+     *      if false, then it's a downward splash.
      */
     function splash(x, strength, isPositive) {
         _swell.start(x, strength, isPositive);
     }
 
     /**
+     * Grab the height of the wave at the given sample index.
+     *
      * @param {number} idx - Index of sample to grab the height for
-     * @return {number} y coordinate for the wave at the sample idx
+     * @return {number} y coordinate for the wave at the sample idx. Returns a
+     *      Float in [-1, 1], where 0 is the neutral level.
      */
     function height(idx) {
         return u[_clamp(idx, 0, _n - 1)];
     };
 
-    /**
-     * Computes the height of the wave at sample i.
-     * @private
-     * @param {number} i - index of the sample to compute the wave height for
-     * @param {nubmer} crestIdx - index of the crest of the wave
-     * @param {WaveSettings} settings
-     */
-    function _childHeight(i, childWaveSettings) {
-        var x, swellX, phaseShift;
-        x = i / _n;
-        swellX = _swell.x();
-        phaseShift = _swell.phaseShift();
-
-        // returns the y value of the child wave at sample i, shifted so that
-        // a peak occurs at the current well's index
-        var amplitude = childWaveSettings.amplitude;
-        var period = childWaveSettings.period;
-        return _swell.strength() * amplitude * Math.sin((2 * Math.PI / period) * (x - phaseShift));
-    };
 
     return {
+        // actions
         tick: tick,
-        height: height,
-        splash: splash
+        splash: splash,
+
+        // getters
+        height: height
     }
 }
 
+/**
+ * Defines a swell in the wave. Keeps track of the current strength
+ * of the swell, which is computed via quad out easing.
+ */
 function swell() {
+    /** CONSTANTS **/
     var DEFAULT_TICKS = 100;
     var MAX_STRENGTH = 10; // [1.0, 10.0], units: permille
+
+    /** PRIVATE VARIABLES **/
     var _x = 0,
         _strength = 0,
         _phaseShift = 0,
@@ -196,6 +217,15 @@ function swell() {
 
     var _easingCoefficient = 0;
         
+    /**
+     * Initialize a new swell.
+     *
+     * @param {number} x - The x position of the swell, a float between [0, 1]
+     * @param {number} strength - The strength of the swell, float between [0, 10]
+     * @param {boolean=} isPositive - if true, then it's an upward swell.
+     *      if false, then it's a downward swell.
+     * @param {number=} ticks - the number of ticks till the swell runs its course
+     */
     function start(x, strength, isPositive, ticks) {
         _gaining = true;
 
@@ -204,14 +234,33 @@ function swell() {
         if (!isPositive) {
             _strength *= -1;
         }
+
+        // compute shift in phase so that the upwards peak is at the given x
         _phaseShift = _x - Math.PI * 0.5;
 
+        // reset ticks
         _currTick = 0;
         _ticks = ticks || DEFAULT_TICKS;
 
         _easingCoefficient = -4 * _strength / (_ticks * _ticks);
     };
 
+    /**
+     * Advance the swell by one tick. Resets the swell if the swell has run
+     * its course.
+     */
+    function tick() {
+        _currTick++;
+        if (_currTick > _ticks) {
+            _gaining = false;
+            _x = 0;
+            _strength = 0;
+            _phaseShift = 0;
+            _currTick = 0;
+            _ticks = 0;
+        }
+    }
+    
     function x() {
         return _x;
     };
@@ -229,18 +278,7 @@ function swell() {
         return _gaining;
     };
 
-    function tick() {
-        _currTick++;
-        if (_currTick > _ticks) {
-            _gaining = false;
-            _x = 0;
-            _strength = 0;
-            _phaseShift = 0;
-            _currTick = 0;
-            _ticks = 0;
-        }
-    }
-    
+
     return {
         // actions
         start: start,
